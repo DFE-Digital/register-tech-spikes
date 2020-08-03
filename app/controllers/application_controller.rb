3f2a9c8dbab1061df70a8c20e676985bc7b24035
Graham Pengelly <graham@three-tier.com>
2020-08-03 17:49:48 +0100

    Add basic authentication
    Authenticate against environment variables
    
    USERNAME
    PASSWORD
    
    Disabled in development.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with name: ENV.fetch("USERNAME", SecureRandom.uuid), password: ENV.fetch("PASSWORD", SecureRandom.uuid) unless Rails.env.development?
 end
