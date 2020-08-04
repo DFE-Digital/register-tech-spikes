aba2f4f2fc8e1ed7abc2395d32f33e889fae11aa
Graham Pengelly <graham@three-tier.com>
2020-08-04 17:42:55 +0100

    Consume OData as JSON
    Spike on interacting with the OData service as a plain JSON API.
    
    All of the basic CRUD operations are working.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,20 +2,20 @@ class PeopleController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    @people = DynamicsService::List.call
+    @people = PersonService::List.call
   end
 
   def edit
-    @person = DynamicsService::Fetch.call(person_id: params[:person_id])
+    @person = PersonService::Fetch.call(person_id: params[:person_id])
   end
 
   def update
-    @person = DynamicsService::Update.call(person_id: params[:person_id], person_name: params[:person_name])
+    @person = PersonService::Update.call(person_id: params[:person_id], person_name: params[:person_name])
     redirect_to root_path
   end
 
   def create
-    @person = DynamicsService::Create.call(person_name: params[:person_name])
+    @person = PersonService::Create.call(person_name: params[:person_name])
     redirect_to root_path
   end
 end
